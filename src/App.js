38d510b1e31ff8cc371348c97888b0bd6534d7e3
thanhyou00 <truc06082000@gmail.com>
2022-04-28 23:42:17 +0700

    delete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.scss';
 import TodoApp from './components/TodoApp';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 function App() {
   return (
+    <Scrollbars style={{ width: '100%', height: '100vh' }}>
     <div className="App">
       <TodoApp />
     </div>
+    </Scrollbars>
   );
 }
 
--- src/components/TodoApp/index.jsx
+++ src/components/TodoApp/index.jsx
@@ -1,10 +1,10 @@
-import CampaignOutlinedIcon from '@mui/icons-material/CampaignOutlined';
 import ClearOutlinedIcon from '@mui/icons-material/ClearOutlined';
+import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import InputBase from '@mui/material/InputBase';
 import Paper from '@mui/material/Paper';
 import { alpha, styled } from '@mui/material/styles';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './todoApp.scss';
 
 const Search = styled('div')(({ theme }) => ({
@@ -46,41 +46,86 @@ const Search = styled('div')(({ theme }) => ({
     color: theme.palette.text.secondary,
   }));
 
+
 function TodoApp() {
+    const [list, setList] = useState([]);
+    function handleFetchData(url) {
+      fetch(url)
+      .then( response =>{
+        return response.json( );
+      })
+      .then( data =>{
+        setList(data)
+      });   
+    }
+    useEffect(()=>{
+        async function fetchList() {
+          try {
+            const url = 'https://626a10a353916a0fbdf4db6d.mockapi.io/reactTodo';
+            handleFetchData(url)
+          } catch (error) {
+            console.log('Faild to fetch ', error.message);
+          }
+        }
+        fetchList();
+    },[])
+    async function handleDelete(id) {
+      const url = 'https://626a10a353916a0fbdf4db6d.mockapi.io/reactTodo';
+     await fetch(url+'/'+id, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+    })
+    .then(response =>{
+      return response.json()
+    })
+    .then(data => 
+        console.log(data) 
+    );
+    // refetch data before delete
+      handleFetchData(url);
+    }
     return (
         <div className='todo-app'>
             <Grid container >
                 <Grid item xs={12}>
             <h4>Welcome to my TodoAPP</h4>
-            <div className='input-search'>
-            <Search>
-            <StyledInputBase
-              placeholder="Type here to search ..."
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
+            <Grid container >
+              <Grid item xs={12} md={6}>
+              <div className='input-search'>
+              <Search>
+              <p style={{fontSize:'1.25rem', marginLeft:'2.188rem', fontWeight:'bolder'}}>Search</p>
+              <StyledInputBase
+                placeholder="Type here to search ..."
+                inputProps={{ 'aria-label': 'search' }}
+              />
+              </Search>
             </div>
+              </Grid>
+              <Grid item xs={12} md={6}>
+              <div className='input-search'>
+              <Search>
+              <p style={{fontSize:'1.25rem', marginLeft:'2.188rem', fontWeight:'bolder'}}>Add a new todo</p>
+              <StyledInputBase
+                inputProps={{ 'aria-label': 'search' }}
+              />
+              <span style={{marginLeft: '1rem'}}><Button variant="contained">Add</Button></span>
+              </Search>
+            </div>
+              </Grid>
+            </Grid>
             <Grid container >
                 <Grid item xs={12}>
                     <Item>
                 <div className='list-items'>
                 <ul style={{listStyle: 'none'}}>
-                    <li> <CampaignOutlinedIcon /> 
-                    Hello
-                    <span style={{float: 'right'}}> <ClearOutlinedIcon /> </span>
-                    </li>
-                    <li> <CampaignOutlinedIcon /> Hello
-                    <span style={{float: 'right'}}> <ClearOutlinedIcon /> </span>
-                    </li>  
-                    <li> <CampaignOutlinedIcon /> Hello
-                    <span style={{float: 'right'}}> <ClearOutlinedIcon /> </span>
-                    </li>  
-                    <li> <CampaignOutlinedIcon /> Hello
-                    <span style={{float: 'right'}}> <ClearOutlinedIcon /> </span>
-                    </li>  
-                    <li> <CampaignOutlinedIcon /> Hello
-                    <span style={{float: 'right'}}> <ClearOutlinedIcon /> </span>
-                    </li>                  
+                  {list.map(x=>(
+                    <li key={x.id}> 
+                    {x.title}
+                    <span style={{float: 'right'}} onClick={()=>{handleDelete(x.id)}}> <ClearOutlinedIcon /> </span>
+                    </li> 
+                  ))}               
                 </ul>
                 </div> 
                 </Item>                   
